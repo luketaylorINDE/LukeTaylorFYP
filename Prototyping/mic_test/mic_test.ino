7c60b84d72089bc264d145e684b2bf756e723f7e
luke taylor <luketaylor96@aol.com>
2018-04-23 15:05:28 +0100

    added tests for mic

---
+++ Prototyping/mic_test/mic_test.ino
@@ -0,0 +1,32 @@
+const int sampleWindow = 50;
+unsigned int sample;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long startMillis = millis();
+  unsigned int peakToPeak = 0;
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  while (millis() - startMillis < sampleWindow) {
+    sample = analogRead(0);
+    if (sample < 1024) {
+      if (sample > signalMax) {
+        signalMax = sample;
+      }
+      else if (sample < signalMin) {
+        signalMin = sample;
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;
+  double volts = (peakToPeak * 5.0) / 1024;
+
+  //Serial.println(volts);
+  Serial.println (peakToPeak);
+}
+
